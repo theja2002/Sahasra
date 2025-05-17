cbd53868755223d1a957fd483e5be72ea89cf061
chennuri Theja <chennuritheja@gmail.com>
2025-05-17 18:40:22 +0530

    concave

--- concave.sh
+++ concave.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-echo "lakshmi::arjun is worst"
-echo "raman::teja is good"
-echo "lakshmi::arjun is good only"
-echo "ramana:: arjun is bad"
+Person1=$person1
+person2=$person2
+person3=$person3
+
+echo "$person1::arjun is worst"
+echo "$person3::teja is good"
+echo "$person1::arjun is good only"
+echo "$person2:: arjun is bad"
