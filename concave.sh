f32df980430766661427437fe120eac84ade6f77
chennuri Theja <chennuritheja@gmail.com>
2025-05-17 18:49:28 +0530

    concave

--- concave.sh
+++ concave.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-Person1=$person1
-person2=$person2
-person3=$person3
-
-echo "$person1::arjun is worst"
-echo "$person3::teja is good"
-echo "$person1::arjun is good only"
-echo "$person2:: arjun is bad"
+echo "tej::arjun will go out"
+echo "arjun:: where we have to"
+echo "tej::arjun will go for cofee"
+echo "arjun::tej sorry i had"
+echo "tej::come give company"
+echo "arjun::okay coming"
