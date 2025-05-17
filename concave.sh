cbf3eb4ea692a24aacde5bddd9e1306e1f927de9
chennuri Theja <chennuritheja@gmail.com>
2025-05-17 18:55:56 +0530

    concave

--- concave.sh
+++ concave.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-perosn1=$raj
+perosn1=raja
 person2=arun
-echo "$raj::arun will go out"
+echo "raja::arun will go out"
 echo "arun:: where we have to"
-echo "$raj::arun will go for cofee"
-echo "arun::$raj sorry i had"
-echo "$raj::come give company"
+echo "raja::arun will go for cofee"
+echo "arun::raja sorry i had"
+echo "raja::come give company"
 echo "arun::okay coming"
